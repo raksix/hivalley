f46188c951d319ef28ad868d80fec6550b12423d
raksix <ermagfurkan7@gmail.com>
2026-07-31 19:40:44 +0300

    Adjust cursor offset for better alignment in MainMenuScene

--- src/scenes/MainMenuScene.js
+++ src/scenes/MainMenuScene.js
@@ -261,7 +261,7 @@ export class MainMenuScene extends Phaser.Scene {
       .setScale(3)
       .setDepth(9999);
     this.input.on('pointermove', (p) => {
-      this.cursorImg.setPosition(p.x + 12, p.y + 4);
+      this.cursorImg.setPosition(p.x + 120, p.y + 50);
     });
     this.input.on('pointerdown', () => {
       this.cursorImg.setTexture('gen:btn-hover');
